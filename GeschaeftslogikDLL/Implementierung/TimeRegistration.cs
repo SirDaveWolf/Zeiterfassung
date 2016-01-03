0bd39da3ea75e57461dbc073cf3c21f8f53051a0
SirDaveWolf <david_maciejewski@web.de>
2016-01-04 00:11:11 +0100

    iwas

--- GeschaeftslogikDLL/Implementierung/TimeRegistration.cs
+++ GeschaeftslogikDLL/Implementierung/TimeRegistration.cs
@@ -9,5 +9,24 @@ namespace Projektarbeit.GeschaeftslogikDLL.Implementierung
 {
     public class TimeRegistration : ITimeRegistration
     {
+        public bool UpdateWorkTime ( Guid userIdentification, DatenhaltungEF.Model.WorkTime workTime, int projectID )
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<DatenhaltungEF.Model.WorkTime> GetMyTimesForProject ( Guid userIdentification, string projectName )
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, List<DatenhaltungEF.Model.WorkTime>> GetMyWorkTimes ( Guid userIdentification )
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<DatenhaltungEF.Model.Project> GetMyProjects ( Guid userIdentification )
+        {
+            throw new NotImplementedException();
+        }
     }
 }
