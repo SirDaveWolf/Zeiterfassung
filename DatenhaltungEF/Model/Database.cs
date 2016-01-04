686e52167e67c5c09cf28155ecace54694936785
SirDaveWolf <david_maciejewski@web.de>
2016-01-04 21:30:08 +0100

    Buuhjaaa, Database!!!

--- DatenhaltungEF/Model/Database.cs
+++ DatenhaltungEF/Model/Database.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Projektarbeit.DatenhaltungEF.Model
 {
-    public class Database 
+    public class Database : DbContext 
     {
+        public virtual
+        DbSet<User> Users { get; set; }
+
+        public virtual
+        DbSet<Project> Projects { get; set; }
+
+        public virtual
+        DbSet<WorkTime> WorkTimes { get; set; }
     }
 }
