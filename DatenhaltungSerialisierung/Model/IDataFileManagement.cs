775a9a8b155cf8d775ad0ba51906c921690c06aa
SirDaveWolf <david_maciejewski@web.de>
2016-01-06 22:01:39 +0100

    IDataFileManagement.cs
    sada

---
+++ DatenhaltungSerialisierung/Model/IDataFileManagement.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projektarbeit.DatenhaltungSerialisierung.Model
+{
+    public interface IDataFileManagement
+    {
+        IUser ReadUser ( String EMail );
+
+        IProject ReadProject ( String description );
+
+        IWorkTime ReadWorkTime ( Int32 id );
+
+        Boolean WriteUser ( IUser user );
+
+        Boolean WriteProject ( IProject project );
+
+        Boolean WriteWorkTime ( IWorkTime workTime );
+
+        Boolean DeleteUser ( String EMail );
+
+        Boolean DeleteProject ( String description );
+
+        List<IUser> GetAllUsers ();
+
+        List<IProject> GetAllProjects ();
+
+        Boolean PermitUserToProject ( String EMail, String description );
+
+        Boolean DenyUserToProject ( String EMail, String description );
+
+
+        List<IWorkTime> GetMyTimesForProject ( String EMail, String description );
+
+        Boolean AssignWorkTimeToUser ( String mail, Guid workTimeID, String projectDescription );
+    }
+}
